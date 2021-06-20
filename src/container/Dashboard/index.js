fee8fcc203420ec946259e561616b6c9a15f834b
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2021-06-21 00:47:43 +0100

    created dashboard and signle collection

--- src/container/Dashboard/index.js
+++ src/container/Dashboard/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Box, Text, Flex, Icon, Button,CircularProgress } from '@chakra-ui/react';
+import { Box, Text, Flex, Icon, Button,CircularProgress, useDisclosure } from '@chakra-ui/react';
 import { HiCheckCircle } from "react-icons/hi";
 import Nav from '../../component/navigation';
 import style from './style.module.css';
 import { BsCollectionFill} from "react-icons/bs";
 import { AiOutlinePlus } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import CreateCollection from '../../component/create_collection';
 
 const Dashboard = () =>{
+    const { isOpen: isOpenCollectionModal, onOpen: onOpenCollectionModal, onClose : onCloseCollectionModal } = useDisclosure();
 
-
-    return <Box justifyContent="center" alignItems="center" backgroundColor="rgb(24, 24, 32)" minHeight="100vh" width="100%" paddingBottom="30px">
+    return <Box justifyContent="center" overflowX="hidden" alignItems="center" backgroundColor="rgb(24, 24, 32)" minHeight="100vh" width="100%" paddingBottom="30px">
        
        <Nav />
 
+        <CreateCollection isOpenCollectionModal={isOpenCollectionModal} onOpenCollectionModal={onOpenCollectionModal} onCloseCollectionModal={onCloseCollectionModal}/>
 
         <Flex flexDirection="column" width="728px"  m="50px auto 0px" className={style.collection_container}>
                 <Text color="white" ml="20px" fontWeight="bold" mb="35px" fontSize="30px">Collections</Text>
@@ -82,7 +84,7 @@ const Dashboard = () =>{
                         </Flex>
                     </Box>
 
-                    <Button id={style.add_collection_box}>
+                    <Button onClick={onOpenCollectionModal} id={style.add_collection_box}>
                         <Icon as={AiOutlinePlus} color="#3d3f4c" fontSize="25px"/>
                     </Button>
                 </Flex>
