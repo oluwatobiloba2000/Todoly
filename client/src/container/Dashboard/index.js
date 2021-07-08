f05648ab13b74550e7c1f484207cf83f238c115a
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2021-07-08 23:00:41 +0100

    created containers that houses of components

--- client/src/container/Dashboard/index.js
+++ client/src/container/Dashboard/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Box, Text, Flex, Icon, Button,CircularProgress, useDisclosure } from '@chakra-ui/react';
-import { HiCheckCircle } from "react-icons/hi";
+import { Box, Text, Flex , useDisclosure } from '@chakra-ui/react';
 import Nav from '../../component/navigation';
 import style from './style.module.css';
-import { BsCollectionFill} from "react-icons/bs";
-import { AiOutlinePlus } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+
 import CreateCollection from '../../component/create_collection';
+import Collections from '../../component/collections';
+import { getTokenFromLocalStorage, isTokenExpired } from '../../utils/auth';
+import { useHistory } from 'react-router-dom';
 
 const Dashboard = () =>{
     const { isOpen: isOpenCollectionModal, onOpen: onOpenCollectionModal, onClose : onCloseCollectionModal } = useDisclosure();
+    const history = useHistory();
+
+    const TokenExpired = isTokenExpired();
+    const token = getTokenFromLocalStorage();
+    if(TokenExpired){
+        history.push(`/?rdr${window.location.pathname}`);
+        return<></>; 
+    }
+    
 
     return <Box justifyContent="center" overflowX="hidden" alignItems="center" backgroundColor="rgb(24, 24, 32)" minHeight="100vh" width="100%" paddingBottom="30px">
        
@@ -20,80 +29,8 @@ const Dashboard = () =>{
         <Flex flexDirection="column" width="728px"  m="50px auto 0px" className={style.collection_container}>
                 <Text color="white" ml="20px" fontWeight="bold" mb="35px" fontSize="30px">Collections</Text>
 
-                <Flex flexWrap="wrap"  spacing={10}>
-
-                    <Link to="/collection/1" className={style.collection_box_link}>
-                        <Box transition="all 250ms" className={style.collection_box}>
-                                <Box backgroundColor="hsl(301deg 78% 38%)" className={style.collection_box_icon_container}>
-                                    <Icon as={BsCollectionFill} color="white" fontSize="25px"/>
-                                </Box>
-
-                                <Text noOfLines={2} className={style.collection_name}>Great test collection</Text>
-
-                                <Flex justifyContent="space-between" alignItems="center" mt="8px">
-                                <Text className={style.collection_done_status}>0/2 done</Text>
-        
-                                    <CircularProgress value={0} size="20px" color="hsl(301deg 78% 38%)"  thickness="14px" />
-                                </Flex>
-                            </Box>
-                    </Link>
-
-                    <Link to="/collection/1" className={style.collection_box_link}>
-                        <Box transition="all 250ms" className={style.collection_box}>
-                    
-                            <Box backgroundColor="hsl(35deg 61% 48%)" className={style.collection_box_icon_container}>
-                                <Icon as={BsCollectionFill} color="white" fontSize="25px"/>
-                            </Box>
-
-                            <Text noOfLines={2} className={style.collection_name}>Tasty Meals Collection</Text>
-                        
-                            <Flex justifyContent="space-between" alignItems="center" mt="8px">
-                                <Text className={style.collection_done_status}>8/8 done</Text>
-
-                                {/* <CircularProgress value={50} size="20px" color="hsl(35deg 61% 48%)"  thickness="14px" /> */}
-                                    
-                                <Icon as={HiCheckCircle} color="hsl(35deg 61% 48%)" fontSize="22px"/>
-                            </Flex>
-                        </Box>                    
-
-                    </Link>
-
-                    <Link to="/collection/1" className={style.collection_box_link}>
-                        <Box transition="all 250ms" className={style.collection_box}>
-                            <Box backgroundColor="hsl(107deg 78% 38%)" className={style.collection_box_icon_container}>
-                                <Icon as={BsCollectionFill} color="white" fontSize="25px"/>
-                            </Box>
-                            <Text noOfLines={2} className={style.collection_name}>Study</Text>
-
-                            <Flex justifyContent="space-between" alignItems="center" mt="8px">
-                            <Text className={style.collection_done_status}>1/8 done</Text>
-    
-                                <CircularProgress value={10} size="20px" color="hsl(107deg 78% 38%)"  thickness="14px" />
-                            </Flex>
-                        </Box>                        
-                    </Link>
-
-                    <Link to="/collection/1" className={style.collection_box_link}>
-                        <Box transition="all 250ms" className={style.collection_box}>
-                            <Box backgroundColor="hsl(182deg 78% 38%)" className={style.collection_box_icon_container}>
-                                <Icon as={BsCollectionFill} color="white" fontSize="25px"/>
-                            </Box>
-
-                            <Text noOfLines={2}  className={style.collection_name}>Private project</Text>
-                        
-                            <Flex justifyContent="space-between" alignItems="center" mt="8px">
-                            <Text className={style.collection_done_status}>3/8 done</Text>
-    
-                                <CircularProgress value={30} size="20px" color="hsl(182deg 78% 38%)"  thickness="14px" />
-                            </Flex>
-                        </Box>                        
-                    </Link>
-
-                    <Button onClick={onOpenCollectionModal} id={style.add_collection_box}>
-                        <Icon as={AiOutlinePlus} color="#3d3f4c" fontSize="25px"/>
-                    </Button>
-                </Flex>
-
+              
+            <Collections onOpenCollectionModal={onOpenCollectionModal} token={token}/>
         </Flex>
 
         {/* <IconButton aria-label="Add todo" icon={<SearchIcon />} /> */}
